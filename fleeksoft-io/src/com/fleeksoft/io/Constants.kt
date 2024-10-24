f2757d09e1cf779732828a6945c8fe829b0b4564
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-10-24 10:32:52 +0500

    Add new buffer size constants
    Introduced IS_DEFAULT_BYTE_BUFFER_SIZE and IS_MAX_SKIP_BUFFER_SIZE in Constants.kt. These will help manage default and maximum buffer sizes more effectively.

--- fleeksoft-io/src/com/fleeksoft/io/Constants.kt
+++ fleeksoft-io/src/com/fleeksoft/io/Constants.kt
@@ -3,6 +3,8 @@ package com.fleeksoft.io
 object Constants {
     const val DEFAULT_CHAR_BUFFER_SIZE: Int = 8192
     const val DEFAULT_BYTE_BUFFER_SIZE: Int = 8192
+    const val IS_DEFAULT_BYTE_BUFFER_SIZE: Int = 16384
+    const val IS_MAX_SKIP_BUFFER_SIZE: Long = 2048
 
     /** Maximum skip-buffer size  */
     const val maxSkipBufferSize: Int = 8192
