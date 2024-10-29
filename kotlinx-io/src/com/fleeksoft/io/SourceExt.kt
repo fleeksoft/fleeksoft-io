00aaa89e9acd1e919476a3aa601cfd3a9bb5434a
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-10-29 17:42:47 +0500

    Add `asInputStream` extension for RawSource

--- kotlinx-io/src/com/fleeksoft/io/SourceExt.kt
+++ kotlinx-io/src/com/fleeksoft/io/SourceExt.kt
@@ -2,6 +2,8 @@ package com.fleeksoft.io
 
 import kotlinx.io.RawSource
 import kotlinx.io.Source
+import kotlinx.io.buffered
 
 public fun Source.asInputStream(): InputStream = SourceInputStream(this)
+public fun RawSource.asInputStream(): InputStream = SourceInputStream(this.buffered())
 public fun InputStream.asSource(): RawSource = InputStreamSource(this)
