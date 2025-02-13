74d74f90a39abcc591ddbf0ecf5f85dc6ddbc277
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2025-02-14 01:48:56 +0500

    add OutputStream

---
+++ io-core/src/com/fleeksoft/io/OutputStream.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 1994, 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.fleeksoft.io
+
+expect abstract class OutputStream() : Closeable, Flushable {
+
+ /**
+     * Writes the specified byte to this output stream. The general
+     * contract for {@code write} is that one byte is written
+     * to the output stream. The byte to be written is the eight
+     * low-order bits of the argument {@code b}. The 24
+     * high-order bits of {@code b} are ignored.
+     *
+     * @param      b   the {@code byte}.
+     * @throws     com.fleeksoft.io.exception.IOException  if an I/O error occurs. In particular,
+     *             an {@code IOException} may be thrown if the
+     *             output stream has been closed.
+     */
+    abstract fun write(b: Int)
+
+
+    /**
+     * Writes {@code b.length} bytes from the specified byte array
+     * to this output stream. The general contract for {@code write(b)}
+     * is that it should have exactly the same effect as the call
+     * {@code write(b, 0, b.length)}.
+     *
+     * @param      b   the data.
+     * @throws     com.fleeksoft.io.exception.IOException  if an I/O error occurs.
+     */
+    open fun write(b: ByteArray)
+
+    open fun write(b: ByteArray, off: Int, len: Int)
+
+    override fun flush()
+
+    override fun close()
+}
