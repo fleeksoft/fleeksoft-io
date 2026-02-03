6a6011e22745387d27d8d24e1ab1bbdae05c42c0
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2026-02-03 22:50:27 +0500

    chore: API level check for Buffer extensions

--- io-core/src@android/com/fleeksoft/io/Buffer.android.kt
+++ io-core/src@android/com/fleeksoft/io/Buffer.android.kt
@@ -1,5 +1,7 @@
 package com.fleeksoft.io
 
+import android.os.Build
+
 actual typealias Buffer = java.nio.Buffer
 
 
@@ -31,12 +33,14 @@ actual fun Buffer.resetExt(): Buffer {
     return this.reset()
 }
 
+@androidx.annotation.RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 actual fun Buffer.sliceExt(): Buffer {
     if (this is CharBuffer) return this.slice()
     if (this is ByteBuffer) return this.slice()
     return this.slice()
 }
 
+@androidx.annotation.RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 actual fun Buffer.sliceExt(index: Int, length: Int): Buffer {
     return this.slice(index, length)
 }
